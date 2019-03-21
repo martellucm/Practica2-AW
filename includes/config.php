579f99de29f1c00b7b7e5deff28496657a347911
MyChusterChuster <38526359+MyChusterChuster@users.noreply.github.com>
2019-03-21 18:51:49 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -6,9 +6,9 @@ require_once __DIR__.'/Aplicacion.php';
  * Parámetros de conexión a la BD
  */
 define('BD_HOST', 'localhost');
-define('BD_NAME', 'ejercicio3');
-define('BD_USER', 'ejercicio3');
-define('BD_PASS', 'ejercicio3');
+define('BD_NAME', 'mychustergames');
+define('BD_USER', 'mychustergames');
+define('BD_PASS', 'mychustergames');
 
 /**
  * Configuración del soporte de UTF-8, localización (idioma y país) y zona horaria
