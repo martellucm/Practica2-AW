ebe3902ea9cae63a970d3dd082afecf60363a46a
MyChusterChuster <38526359+MyChusterChuster@users.noreply.github.com>
2019-03-21 18:49:33 +0100

    Creación de tablas hecho

--- mysql/02-tablas.sql
+++ mysql/02-tablas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-03-2019 a las 17:35:42
--- Versión del servidor: 10.1.30-MariaDB
--- Versión de PHP: 7.2.1
+-- Tiempo de generación: 21-03-2019 a las 18:47:42
+-- Versión del servidor: 10.1.35-MariaDB
+-- Versión de PHP: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,8 +21,34 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `mychustergames`
 --
-CREATE DATABASE IF NOT EXISTS `ejercicio3` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `mychustergames`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `foto`
+--
+
+CREATE TABLE `foto` (
+  `idProd` int(11) NOT NULL,
+  `foto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(11) NOT NULL,
+  `nombreProd` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
+  `puntos` int(10) NOT NULL DEFAULT '0',
+  `descript` text COLLATE utf8_spanish_ci NOT NULL,
+  `edad` int(10) NOT NULL DEFAULT '0',
+  `jugadores` int(10) NOT NULL DEFAULT '0',
+  `link` text COLLATE utf8_spanish_ci NOT NULL,
+  `empresa` varchar(20) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -35,9 +61,9 @@ CREATE TABLE `usuarios` (
   `nombreUsuario` varchar(15) NOT NULL,
   `nombre` varchar(40) NOT NULL,
   `email` varchar(80) NOT NULL,
-  `ptosForum` int(10) DEFAULT 0,
-  `ptosProd` int(10) DEFAULT 0,
-  `ptosTourn` int(10) DEFAULT 0,
+  `ptosForum` int(10) DEFAULT '0',
+  `ptosProd` int(10) DEFAULT '0',
+  `ptosTourn` int(10) DEFAULT '0',
   `password` varchar(80) NOT NULL,
   `avatar` varchar(20) NOT NULL,
   `rol` varchar(10) NOT NULL
@@ -48,16 +74,44 @@ CREATE TABLE `usuarios` (
 --
 
 --
+-- Indices de la tabla `foto`
+--
+ALTER TABLE `foto`
+  ADD PRIMARY KEY (`idProd`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`nombreProd`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombreUsuario` (`nombreUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `foto`
+--
+ALTER TABLE `foto`
+  ADD CONSTRAINT `foto_ibfk_1` FOREIGN KEY (`idProd`) REFERENCES `producto` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
