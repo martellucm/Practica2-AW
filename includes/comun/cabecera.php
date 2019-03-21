45687a78c62f3ccab13db471420d8eda3ecd7c87
MyChusterChuster <38526359+MyChusterChuster@users.noreply.github.com>
2019-03-21 20:10:28 +0100

    none page

--- includes/comun/cabecera.php
+++ includes/comun/cabecera.php
@@ -4,20 +4,20 @@
 	<?php
 	if (isset($_SESSION["login"]) && ($_SESSION["login"]===true)) {
 		echo "Bienvenido, " . $_SESSION['nombre'] . ".<a href='logout.php'>(salir)</a>";
-		
+
 	} else {
 		echo "<a href='login.php'>Login</a>";
 		echo "<a href='registro.php'>Registro</a>";
 	}
 	?>
-	
+
 	<div class="navMenu">
 		<ul>
 			<li><a href='index.php'>Home</a></li>
 			<li><a href=''>Productos</a></li>
-			<li><a href=''>Torneos</a></li>
-			<li><a href=''>About us</a></li>
-			<li><a href=''>Foro</a></li>
+			<li><a href='none_page.php'>Torneos</a></li>
+			<li><a href='none_page.php'>About us</a></li>
+			<li><a href='none_page.php'>Foro</a></li>
 		</ul>
 		</div>
 	</div>
