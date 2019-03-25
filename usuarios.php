e64529b992f6acf51aed368ce534ab3c50a9bca6
MyChusterChuster <38526359+MyChusterChuster@users.noreply.github.com>
2019-03-25 18:51:51 +0100

    Update usuarios.php

--- usuarios.php
+++ usuarios.php
@@ -16,14 +16,21 @@ require_once __DIR__.'/includes/Usuario.php';
 			$user = Usuario::buscaUsuarioID($_GET['id']);
 			if($user instanceof Usuario){
 				echo '<div>';
-				echo '<div><img src="data:image/jpg; base64,'.base64_encode($user->fprincipal()).'" /></div>';
-				
+        $id = $user->id();
+          $directorio = "img/users/$id.jpg";
+          if(file_get_contents($directorio) == null){
+                  echo '<div><img src="img/users/default_user.png"/></div> ';
+              }
+              else{
+                  echo '<div id = "img_user"><img src='.$directorio.'></div>';
+              }
+
 				echo '<div class="user_nick"><h1>Nickname: '.$user->nombreUsuario().'</h1></div>';
 				echo '<div class="user_name"><h2>Nombre completo: '.$user->nombre().'</h2></div>';
 				echo '<div class="user_rol"><h2>Rol: '.$user->rol().'</h2></div>';
 				echo '<div class="user_emai"><h3>Email: '.$user->email().'</h3></div>';
 				echo '<div class="user_birt"><h3>Fecha de nacimiento: '.$user->cumple().'</h3></div>';
-			  
+
 				echo '<div class="user_poin"><p>Puntos obtenidos en el foro:'.$user->ptosForum().'</p></div>';
 				echo '<div class="user_poin"><p>Puntos obtenidos valorando productos:'.$user->ptosProd().'</p></div>';
 				echo '<div class="user_poin"><p>Puntos obtenidos en torneos'.$user->ptosTourn().'</p></div>';
